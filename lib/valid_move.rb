e27640ebb0a7d057715e0bd5696648a2b2372a41
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-09 20:09:02 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,10 +2,7 @@
 def valid_move?(board, index)
 position = index.to_i - 1
 	if !position_taken?(board, position) && position.between?(0,8)
-		  true
-
-	else
-		false
+		  true : false
 
   end
 end
