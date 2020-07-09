a84f6e3814529e780a56f64d049b9eee40640a77
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-09 19:48:42 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@
   position.to_i.between?(1,9) and not position_taken?(board, position.to_i - 1)
 end
 
-  
+
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
